52e0ef2cf2f0e749c5c655461ff0a82a03ec3332
xiaolinxiang <xiaolinxiang@cndh.top>
2023-06-17 18:25:43 +0800

    第一次提交信息

---
+++ src/pcb_pg/demo.java
@@ -0,0 +1,4 @@
+package pcb_pg;
+
+public class demo {
+}
---
+++ src/pcb_pg/pcb.java
@@ -0,0 +1,24 @@
+package pcb_pg;
+
+public class pcb {
+    public static void main(String[] arg){
+        float Lmax ;
+        float Lmin ;
+
+        float Smax ;
+        float Smin ;
+
+        float Wmax ;
+        float Wmin ;
+
+        float Jt ;
+        float Jh ;
+        float Js;
+
+        float Cl ;
+        float Cs ;
+        float Cw;
+
+
+    }
+}
